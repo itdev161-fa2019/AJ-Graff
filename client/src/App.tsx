3751a9cb38c63a7382c5ba11fc2486ad7f0a87de
BuildTools <unconfigured@null.spigotmc.org>
2019-12-02 20:54:02 -0600

    Created Post API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,22 +7,12 @@ import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
-    data: null,
+    posts: [],
     token: null,
     user: null
-  }
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000')
-    .then((response) => {
-      this.setState({
-        data: response.data
-      })
-    })
-    .catch((error) => {
-      console.error(`Error fetching data: ${error}`);
-    })
-
     this.authenticateUser();
   }
 
@@ -39,19 +29,50 @@ class App extends React.Component {
         headers: {
           'x-auth-token': token
         }
-      }
-      axios.get('http://localhost:5000/api/auth', config)
-      .then((response) => {
-        localStorage.setItem('user', response.data.name)
-        this.setState({ user: response.data.name })
-      })
-      .catch((error) => {
-        localStorage.removeItem('user');
-        this.setState({ user: null });
-        console.error(`Error logging in: ${error}`);
-      })
+      };
+      axios
+        .get('http://localhost:5000/api/auth', config)
+        .then(response => {
+          localStorage.setItem('user', response.data.name);
+          this.setState(
+            {
+              user: response.data.name,
+              token: token
+            },
+            () => {
+              this.loadData();
+            }
+          );
+        })
+        .catch(error => {
+          localStorage.removeItem('user');
+          this.setState({ user: null });
+          console.error(`Error Logging in: ${error}`);
+        });
     }
-  }
+  };
+
+  loadData = () => {
+    const { token } = this.state;
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      };
+      axios
+        .get('http://localhost:5000/api/posts', config)
+        .then(response => {
+          this.setState({
+            posts: response.data
+          });
+        })
+        .catch(error => {
+          console.error(`Error fetching data: ${error}`);
+        });
+    }
+  };
 
   logOut = () => {
     localStorage.removeItem('token');
@@ -60,10 +81,11 @@ class App extends React.Component {
   }
 
   render () {
-    let { user, data } = this.state;
+    let { user, posts } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
-    }
+    };
+
     return (
       <Router>
         <div className="App">
@@ -89,7 +111,14 @@ class App extends React.Component {
               {user ?
                 <React.Fragment>
                   <div>Hello, {user}!</div>
-                  <div>{data}</div>
+                  <div>
+                    {posts.map(post => {
+                      return <div key={post._id}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                      </div>
+                    })}
+                  </div>
                 </React.Fragment> :
                 <React.Fragment>
                   Please Register or Login
